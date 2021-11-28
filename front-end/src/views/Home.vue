e68863b5ce1b667efb26a9b417b92411c2b0429a
Miguelnr <migueljf09@gmail.com>
2021-11-27 23:11:37 -0600

    pagina 2

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="accordion" role="tablist">
-    <b-label>My trailers</b-label>
+    <label>My trailers</label>
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
         <b-button block v-b-toggle.accordion-1 variant="info">Action</b-button>
       </b-card-header>
       <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
         <b-card-body>
-          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
-          <b-card-text>{{ text }}</b-card-text>
+          <ul>
+            <li v-for="(obj, index) in accion" :key="index" @click="sendMovie(obj.id)"><b-link>{{obj.link}}</b-link></li>
+          </ul>
         </b-card-body>
       </b-collapse>
     </b-card>
 
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-2 variant="info">Super heroes</b-button>
+        <b-button block v-b-toggle.accordion-2 variant="info">Terror</b-button>
       </b-card-header>
       <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
         <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
+          <ul>
+            <li v-for="(obj, index) in terror" :key="index" @click="sendMovie(obj.id)"><b-link>{{obj.link}}</b-link></li>
+          </ul>
         </b-card-body>
       </b-collapse>
     </b-card>
@@ -30,7 +33,9 @@
       </b-card-header>
       <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
         <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
+          <ul>
+            <li v-for="(obj, index) in drama" :key="index" @click="sendMovie(obj.id)"><b-link>{{obj.link}}</b-link></li>
+          </ul>
         </b-card-body>
       </b-collapse>
     </b-card>
@@ -43,17 +48,23 @@ export default {
   name: 'Home',
   data() {
       return {
-        text: `
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-          brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-          tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-          vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
-          synth nesciunt you probably haven't heard of them accusamus labore VHS.
-        `
+        accion:[{link:'pelicula 1', id:1},
+        {link:'pelicula 2', id:2},
+        {link:'pelicula 3', id:3}],
+        terror:[{link:'pelicula 1', id:1},
+        {link:'pelicula 2', id:2},
+        {link:'pelicula 3', id:3},
+        {link:'pelicula 4', id:4}],
+        drama:[{link:'pelicula 1', id:1},
+        {link:'pelicula 2', id:2}]
+        
+      }
+    },
+    methods:{
+      sendMovie(id){
+        this.$router.push({ name: 'Pelicula', params: { id: id } })
       }
     }
+    
 }
 </script>
---
+++ front-end/src/views/Pelicula.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-container class="bv-example-row">
+    <b-row>
+        <b-col>Year</b-col>
+        <b-col><b-input type="text" value="2000"/></b-col>
+        <b-col>Director</b-col>
+        <b-col><b-input type="text" value="Steven Spielberg"/></b-col>
+    </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id:''
+        }
+    },
+    created(){
+        this.getId();
+    },
+    methods:{
+        getId(){
+        this.id = this.$route.params.id;
+    }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
